40bfe4eb7c88539c8b7edf1d1086f7514c1827fb
Kevin Downey (hiredman) <redchin@gmail.com>
2010-10-02 02:27:06 -0700

    because it doesn't

--- src/conduit_hornetq/core.clj
+++ src/conduit_hornetq/core.clj
@@ -3,7 +3,9 @@
   (:import (org.hornetq.api.core TransportConfiguration SimpleString)
            (org.hornetq.api.core.client HornetQClient MessageHandler)
            (org.hornetq.core.remoting.impl.netty NettyConnectorFactory)
-           (java.util UUID)))
+           (java.util UUID)
+           (java.io ByteArrayOutputStream ObjectOutputStream
+                    ByteArrayInputStream ObjectInputStream)))
 
 (defn create-session-factory [host port]
   (-> NettyConnectorFactory .getName
@@ -20,15 +22,15 @@
                   true
                   true
                   false
-                  5))
-
-(defn create-queue [session name]
-  (try
-    (.createQueue session name name true)
-    (catch Exception _)))
+                  1))
 
 (defn create-tmp-queue [session name]
   (.createTemporaryQueue session name name))
+
+(defn create-queue [session name]
+  (try
+    (.createQueue session name name)
+    (catch Exception _)))
 
 (defn ack [session msg]
   (.acknowledge msg)
@@ -58,67 +60,106 @@
         (.send @producer msg)
         (fn []
           (let [msg (.receive consumer)]
-            (ack msg)
+            (ack *session* msg)
             [msg hornetq-reply]))))))
 
 (defn reply-fn [f]
-  (partial (fn hornet-reply-fn [f msg]
-             (let [bytes (byte-array (.getBodySize msg))
-                   reply-queue (.getStringProperty msg "replyTo")]
-               (-> msg .getBodyBuffer (.readBytes bytes))
-               (let [producer (.createMessage *session* reply-queue)
-                     [new-bytes new-f] (f bytes)
-                     new-msg (.createMessage *session* true)]
-                 (-> msg .getBodyBuffer (.writeBytes new-bytes))
-                 (.send producer msg)
-                 [[] (partial hornet-reply-fn new-f)])))
+  (partial (fn hornet-reply-fn [f [bytes reply-queue]]
+             (println "reply-fn")
+             (let [producer (.createProducer *session* reply-queue)
+                   [[new-bytes] new-f] (f bytes)
+                   new-msg (.createMessage *session* true)]
+               (-> new-msg .getBodyBuffer (.writeBytes new-bytes))
+               (.send producer new-msg)
+               (.commit *session*)
+               [[] (partial hornet-reply-fn new-f)]))
            f))
 
+(def producer
+  (memoize
+   (fn [session queue]
+     (.createProducer session queue))))
+
 (defn hornetq-pub-reply [queue id]
-  (let [producer (delay (.createProducer *session* queue))]
-    (fn hornetq-reply [bytes]
-      (let [msg (.createMessage *session* true)
-            reply-queue (str queue ".reply." (UUID/randomUUID))
-            consumer (.createConsumer *session* queue)]
-        (create-tmp-queue *session* reply-queue)
-        (-> msg .getBodyBuffer (.writeBytes bytes))
-        (.putStringProperty msg "id" id)
-        (.putStringProperty msg "replyTo" reply-queue)
-        (.send @producer msg)
-        (let [msg (.receive consumer)]
-          (ack msg)
-          [msg hornetq-reply])))))
+  (fn hornetq-reply [bytes]
+    (println "@hornetq-reply" queue id)
+    (println (String. bytes "utf8"))
+    (create-queue *session* queue)
+    (println "after queue")
+    (let [producer (producer *session* queue)
+          msg (.createMessage *session* true)
+          reply-queue (str queue ".reply." (UUID/randomUUID))
+          consumer (.createConsumer *session* queue)]
+      (create-tmp-queue *session* reply-queue)
+      (-> msg .getBodyBuffer (.writeBytes bytes))
+      (.putStringProperty msg "id" id)
+      (.putStringProperty msg "replyTo" reply-queue)
+      (.send producer msg)
+      (.commit *session*)
+      (let [msg (.receive (.createConsumer *session* reply-queue))
+            bytes (byte-array (.getBodySize msg))]
+        (-> msg .getBodyBuffer (.readBytes bytes))
+        (ack *session* msg)
+        [[bytes] hornetq-reply]))))
 
 (defn a-hornetq [queue id proc]
   (assoc proc
     :type :hornetq
     :source queue
     :id (str id)
-    :reply (hornetq-pub-reply queue id)
+    :reply (hornetq-pub-reply queue (str id "-reply"))
     :no-reply (hornetq-pub-no-reply queue id)
-    :scatter-gather (hornetq-sg-fn queue id)
-    :parts (update-in proc [:parts]
-                      conj
-                      [queue {:type :hornetq
-                              (str id) (:no-reply proc)
-                              (str id "-reply") (reply-fn (:reply proc))}])))
+    :scatter-gather (hornetq-sg-fn queue (str id "-reply"))
+    :parts (assoc (:parts proc)
+             queue {:type :hornetq
+                    (str id) (:no-reply proc)
+                    (str id "-reply") (reply-fn (:reply proc))})))
 
-(defn hornetq-run [proc session queue]
-  (create-queue queue)
-  (let [consumer (.createConsumer session queue)]
-    (letfn [(next-msg [queue]
-              [(.receive consumer) next-msg])
-            (handle-msg [f msg]
-              (try
-                (let [new-f (second (f msg))]
-                  (ack msg)
-                  [[] (partial handle-msg new-f)])
-                (catch Exception e
-                  [[] f])))]
-      (binding [*session* session]
+(defn hornetq-run [proc session]
+  (try
+    (.start session)
+    (let [queue (:source proc)
+          _ (create-queue session queue)]
+      (letfn [(next-msg [queue]
+                (fn next-msg-inner [_]
+                  (println "@next-msg-inner")
+                  (let [msg (.receive (.createConsumer session queue))]
+                    (println msg)
+                    [[[(.getStringProperty msg "id")
+                       [msg (.getStringProperty msg "replyTo")]]]
+                     next-msg-inner])))
+              (handle-msg [f msg]
+                (try
+                  (let [bytes (byte-array (.getBodySize (first (second msg))))
+                        _ (-> (second msg) first .getBodyBuffer (.readBytes bytes))
+                        msg-pair (update-in msg [1 0] (constantly bytes))
+                        _ (println msg-pair)
+                        result (f msg-pair)
+                        new-f (second result)]
+                    (ack session (first (second msg)))
+                    [[] (partial handle-msg new-f)])
+                  (catch Exception e
+                    [[] f])))]
+        (binding [*session* session]
           (dorun
            (a-run
             (reduce
              comp-fn
              [(next-msg queue)
-              (partial handle-msg select-fn (-> proc :parts queue))])))))))
+              (partial handle-msg
+                       (partial select-fn
+                                (get-in proc [:parts queue])))]))))))
+    (finally
+     (.stop session))))
+
+(def-arr serialize [object]
+  (with-open [baos (ByteArrayOutputStream.)
+              oos (ObjectOutputStream. baos)]
+    (.writeObject oos object)
+    (.toByteArray baos)))
+
+(def deserialize
+  (a-arr (fn deserialize [bytes]
+           (with-open [bais (ByteArrayInputStream. bytes)
+                       ois (ObjectInputStream. bais)]
+             (.readObject ois)))))
--- test/conduit_hornetq/test/core.clj
+++ test/conduit_hornetq/test/core.clj
@@ -3,6 +3,8 @@
   (:use [clojure.test]
         [conduit.core]))
 
+(def Q (gensym))
+
 (defn hornetq-session []
   (let [sf (create-session-factory "localhost" 5445)]
     (create-session sf "guest" "guest")))
@@ -10,11 +12,45 @@
 (def hornet-proc
   (a-comp
    (a-hornetq
-    "some.q"
+    (str "some.q." Q)
     "foo"
     (a-arr identity))
    pass-through))
 
+(def upcase (a-hornetq
+             (str "some.q." Q)
+             "upcase"
+             (a-comp deserialize
+                     (a-arr #(.toUpperCase %))
+                     serialize)))
+
+(use-fixtures :once
+              (fn [f]
+                (binding [*session* (hornetq-session)]
+                  (let [session *session*]
+                    (future
+                      (try
+                        (hornetq-run upcase session)
+                        (catch Exception e
+                          (.printStackTrace e)))))
+                  (try
+                    (.start *session*)
+                    (f)
+                    (finally
+                     (.stop *session*))))))
+
 (deftest test-a-hornetq
   (let [[result] ((test-conduit-fn hornet-proc) (.getBytes "foo bar"))]
-    (is (= "foo bar" (String. result "utf8")))))
+    (is (= "foo bar" (String. result "utf8"))))
+  (let [[result] ((test-conduit-fn
+                   (a-comp serialize
+                           hornet-proc
+                           deserialize)) "foo bar")]
+    (is (= "foo bar" result)))
+  (let [result (conduit-map
+                (a-comp serialize
+                        upcase
+                        deserialize
+                        pass-through)
+                ["foo"])]
+    (is (= ["FOO"] result))))
